2fa25cb9460d2a8046adadca8082c59c02c544f0
kaduhod <carlosjr.ribas@gmail.com.br>
2025-03-22 20:53:38 -0300

    :hammer: Corrigindo varaiveis lixo

--- assets/js/den_gor.js
+++ assets/js/den_gor.js
@@ -1,4 +1,4 @@
-obj_protocolos_densidade_gordura = [
+const obj_protocolos_densidade_gordura = [
     {
         name:'Jackson e Pullock, 1978',
         id:'Jackson-e-Pullock-1978',
@@ -23,15 +23,15 @@ obj_protocolos_densidade_gordura = [
 ]
 
 function bota_underline_aumenta_font(item_selected){// boto underline e aumento tamanho de fonte do item selecionado no menu de equações de densidade corporal
-   item_selected.parentNode.childNodes.forEach((el,index) => {
-       if(index%2!=0){el.style.border = 'none';el.style.fontSize = '16px'}
-   });
-   item_selected.style.borderBottom = '4px solid white'
-   item_selected.style.fontSize = '18px'
-   
-   conteudo = filtra_arrObj_por_prop_id(obj_protocolos_densidade_gordura,item_selected.getAttribute('id'))
-   troca_conteudo_paragrafo_densidade(conteudo)
-   troca_funcao_botao(item_selected.getAttribute('id'))
+    item_selected.parentNode.childNodes.forEach((el,index) => {
+        if(index%2!=0){el.style.border = 'none';el.style.fontSize = '16px'}
+    });
+    item_selected.style.borderBottom = '4px solid white'
+    item_selected.style.fontSize = '18px'
+
+    const conteudo = filtra_arrObj_por_prop_id(obj_protocolos_densidade_gordura,item_selected.getAttribute('id'))
+    troca_conteudo_paragrafo_densidade(conteudo)
+    troca_funcao_botao(item_selected.getAttribute('id'))
 }
 function troca_conteudo_paragrafo_densidade(conteudo){//troco o conteudo do paragrafo para o texto da equação selecionada
     var titulo = document.getElementById('tittle-dinamic')
@@ -41,8 +41,8 @@ function troca_conteudo_paragrafo_densidade(conteudo){//troco o conteudo do para
 
 }
 function calcular(equacao){
-   dados = verifica_inputs_den_gor()
-   if(dados != false || null){
+    const dados = verifica_inputs_den_gor()
+    if(dados != false || null){
         if(equacao == 'Jackson-e-Pullock-1978'){
             let user = new Dados_Jackson_Pullock_1978(dados[0].valor,parseInt(dados[1].valor),parseFloat(dados[2].valor), parseFloat(dados[4].valor),parseFloat(dados[3].valor))
             user.soma_dobras()
@@ -52,8 +52,8 @@ function calcular(equacao){
                 alert('Está equação é para homens de 18 á 61 anos!')
             }
             bota_resultado_den_gor(user.densidade, user.gordura_corporal)
-       }
-       if(equacao == 'Sloan-masculino'){
+        }
+        if(equacao == 'Sloan-masculino'){
             let user = new Dados_Sloan_Masculino(dados[0].valor, parseInt(dados[1].valor), parseFloat(dados[5].valor), parseFloat(dados[3].valor))
             user.densidade_corporal()
             user.siri()
@@ -62,17 +62,17 @@ function calcular(equacao){
             }
             bota_resultado_den_gor(user.densidade, user.gordura_corporal)
 
-       }
-       if(equacao == 'Sloan-Burt-&-Blyth-feminino'){
-            let user = new Dados_Sloan_Blyth_Burt_Feminino(dados[0].valor, parseInt(dados[1].valor),parseFloat(dados[6].valor), parseFloat(dados[7].valor))    
+        }
+        if(equacao == 'Sloan-Burt-&-Blyth-feminino'){
+            let user = new Dados_Sloan_Blyth_Burt_Feminino(dados[0].valor, parseInt(dados[1].valor),parseFloat(dados[6].valor), parseFloat(dados[7].valor))
             user.densidade_corporal()
             user.siri()
             if(user.sexo != 'F'){
                 alert( 'Está equação é para mulheres!')
             }
             bota_resultado_den_gor(user.densidade, user.gordura_corporal)
-       }
-       if(equacao == 'Guedes-1985'){
+        }
+        if(equacao == 'Guedes-1985'){
             let user = new Dados_guedes(dados[0].valor, parseInt(dados[1].valor), parseFloat(dados[7].valor), parseFloat(dados[6].valor), parseFloat(dados[4].valor), parseFloat(dados[5].valor), parseFloat(dados[3].valor))
             user.densidade_corporal()
             user.siri()
@@ -80,8 +80,8 @@ function calcular(equacao){
                 alert( 'Está equação é para pessoas com idade entre 17 á 27 anos!')
             }
             bota_resultado_den_gor(user.densidade, user.gordura_corporal)
-       }
-   }
+        }
+    }
 }
 function troca_funcao_botao(item_selecionado){
     var botao = document.getElementById('botao-den-gor')
@@ -93,22 +93,22 @@ function verifica_inputs_den_gor(){//Verificação dos inputs
     for(value of inputs_den_gor){// reseto a cor para que o usuario nao seja alertado sobre erro sem necessidade
         value.style.backgroundColor = '#ffffff'
     }
-    inputs_sem_valor = []
-    valores_validos_den_gor = []
+    let inputs_sem_valor = []
+    let valores_validos_den_gor = []
     for(value of inputs_den_gor){//identifico inputs sem valor digitado
         if(value.value.length<1 || value.value < 0 || value.value > 110){// verifico se o valores nos inputs são validos
             value.style.backgroundColor = '#db656141'
             inputs_sem_valor.push(value.getAttribute('id'))//Pego o nome do input e lança para uma lista de erros
         }else{//Caso nao tenha nenhum erro cvou juntar os dados
-            obj = {// este objeto contem os valores dos inputs
+            const obj = {// este objeto contem os valores dos inputs
                 prop: value.getAttribute('id'),
                 valor: value.value
             }
-            valores_validos_den_gor.push(obj)                      
+            valores_validos_den_gor.push(obj)
         }
     }
     if(inputs_sem_valor.length > 0){//Boto mensagem de erro para o usuario caso os inputs nao sejam marcados
-        error_msg = ''
+        let error_msg = ''
         inputs_sem_valor.forEach((el,index)=>{
             if(index == 0){
                 error_msg = el
@@ -117,7 +117,7 @@ function verifica_inputs_den_gor(){//Verificação dos inputs
             }else{
                 error_msg += `, ${el}`
             }
-            
+
         })
         alert(error_msg + ' precisam ter valores válidos para o cálculo!')
         return false
@@ -126,7 +126,7 @@ function verifica_inputs_den_gor(){//Verificação dos inputs
     }
 }
 function filtra_arrObj_por_prop_id(obj,id){
-    retorno = null
+    let retorno = null
     obj.forEach((el)=>{
         if(el.id == id){
             retorno = el
